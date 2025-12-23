b61945cc34a81cddafb6c4381c116c7f139fee88
sednave <sednave>
2025-12-23 12:34:41 -0800

    Created homepage

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Homepage from './routes/Homepage';
+
+function App() {
+  return (
+    <>
+      <Homepage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return <footer>
+        <h2>Y2NES</h2>
+        <div>
+            <h3>Info</h3>
+            <a href="/home">Home</a>
+            <a href="/characters">Characters & References</a>
+            <a href="/about">About</a>
+        </div>
+    </footer>
+}
+
+export default Footer;
