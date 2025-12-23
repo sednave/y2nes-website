2ce8f99af98deecc7942d73f5ffa5b3a13578be9
sednave <sednave>
2025-12-23 12:54:28 -0800

    git

--- src/components/header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import AnnouncementBar from './AnnouncementBar.tsx';
-
-const Header = () => {
-    return <header>
-        <AnnouncementBar></AnnouncementBar>
-    </header>
-}
-
-export default Header;
