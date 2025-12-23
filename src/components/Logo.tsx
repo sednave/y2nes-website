f71aee54a4ee2f129b9dd57c98fd7970d5dfc226
sednave <sednave>
2025-12-23 13:38:46 -0800

    Updated Homepage for accessibility

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,7 @@
 const Logo = () => {
-    return <img src="https://images.squarespace-cdn.com/content/v1/68cb679662889c19257a6a93/4a8e9918-3e84-4c29-8c9d-1fdac2beb051/STARDUST_PHARMACY_logo_2.png?format=1500w" alt="Logo" />;
+    return <a href="/home" className="logo">
+        <img src="https://images.squarespace-cdn.com/content/v1/68cb679662889c19257a6a93/4a8e9918-3e84-4c29-8c9d-1fdac2beb051/STARDUST_PHARMACY_logo_2.png?format=1500w" alt="Y2NES Logo" />
+    </a>;
 }
 
 export default Logo;
--- src/components/header/AnnouncementBar.tsx
+++ src/components/header/AnnouncementBar.tsx
@@ -4,13 +4,9 @@ const AnnouncementBar = () => {
     return <div className="announcement-bar">
         <Logo></Logo>
         <div className="announcement-bar-links">
-            <a href="home">Home</a>
-            <a href="characters">Characters & References</a>
-            <a href="store">Store</a>
-            <a href="https://discord.com/invite/28ZypjypMq">Discord</a>
-            <a href="https://www.youtube.com/@Y2NES">Youtube</a>
-            <a href="https://y2nes.bandcamp.com">Bandcamp</a>
-            <a href="https://open.spotify.com/artist/0j7ysZlkFVCFCYX59vb0hs">Spotify</a>
+            <a href="/home">Home</a>
+            <a href="/characters">Characters & References</a>
+            <a href="/store">Store</a>
         </div>
     </div>
 }
