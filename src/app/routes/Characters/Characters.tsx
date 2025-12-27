2a2b5eb3d13b3fcab0fcf008adbda22b5ec298c4
sednave <sednave>
2025-12-27 14:40:26 -0800

    Made the website look nicer

--- src/app/routes/Characters/Characters.tsx
+++ src/app/routes/Characters/Characters.tsx
@@ -5,6 +5,8 @@ import CharacterIconButton from "./CharacterIconButton";
 import { useState } from 'react';
 import useCharacterInfos from "../../../hooks/useCharacterInfos";
 import CharacterInfoSection from "./CharacterInfoSection";
+import SiteBackground from "../../../components/backgrounds/SiteBackground";
+import Marquee from "react-fast-marquee";
 
 const Characters = () => {
     const [characterId, setCharacterId] = useState(-1);
@@ -19,11 +21,18 @@ const Characters = () => {
     }
 
     const characterPortraits = useCharacterInfos().map((characterInfo) => {
-        return <CharacterIconButton key={characterInfo.id} characterId={characterInfo.id} onClick={() => onCharacterPortraitClicked(characterInfo.id)} />
+        const isSelected = characterInfo.id === characterId
+        return <CharacterIconButton isSelected={isSelected} key={characterInfo.id} characterId={characterInfo.id} onClick={() => onCharacterPortraitClicked(characterInfo.id)} />
     })
     
     return <div className="page">
+        <SiteBackground />
         <Header />
+        <Marquee autoFill={true} className="marquee" gradient={true} gradientColor="black" direction="right" speed={50}>
+            <p>SAY HELLO</p>
+            <p>·</p>
+        </Marquee>
+
         <main id="characters">
             <h1>MEET THE CHARACTERS</h1>
             <div id="character-icons">
@@ -31,6 +40,11 @@ const Characters = () => {
             </div>
             <CharacterInfoSection characterId={characterId} />
         </main>
+
+        <Marquee autoFill={true} className="marquee" gradient={true} gradientColor="black" direction="left" speed={50}>
+            <p>SAY HELLO</p>
+            <p>·</p>
+        </Marquee>
         <Footer />
     </div>
 }
